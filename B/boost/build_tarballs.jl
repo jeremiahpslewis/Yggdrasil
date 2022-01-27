08dae9267d64d854e803de92a842225808cb3ade
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-27 09:26:06 +0100

    Further limit builds

--- B/boost/build_tarballs.jl
+++ B/boost/build_tarballs.jl
@@ -64,7 +64,7 @@ install_license LICENSE_1_0.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> (Sys.islinux(p) && arch(p) == "x86_64"), supported_platforms())
+platforms = filter!(p -> (Sys.islinux(p) && arch(p) == "x86_64" && libc(p) == "glibc"), supported_platforms())
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
@@ -102,6 +102,8 @@ products = Product[
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
 ]
+
+version = v"1.77.0" # Fake version
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"11.1.0", julia_compat="1.6")
